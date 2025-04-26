54f4728d1d0757f61c5df047dc03a501681c6740
shweta khedkar <137026859+shweta-khedkar@users.noreply.github.com>
2025-04-26 12:12:12 +0530

    check bootstrap deployment

--- mit_mumbai_bharat_web/src/components/Header/Header.jsx
+++ mit_mumbai_bharat_web/src/components/Header/Header.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      This is Header
-    </div>
+    <header>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <a className="navbar-brand" href="#">My College</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Courses</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
   )
 }
 

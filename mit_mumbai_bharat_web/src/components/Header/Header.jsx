c0455c45d739d9dfab150f59e8ac649280cf7dac
shweta khedkar <137026859+shweta-khedkar@users.noreply.github.com>
2025-04-26 13:02:19 +0530

    Header created but responsiveness is left

--- mit_mumbai_bharat_web/src/components/Header/Header.jsx
+++ mit_mumbai_bharat_web/src/components/Header/Header.jsx
@@ -1,34 +1,63 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/mit-wpu-logo.png';
+import './header.css'; // Import custom CSS
 
 const Header = () => {
   return (
-    <header>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <a className="navbar-brand" href="#">My College</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Courses</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
+    <header style={{ backgroundColor: '#000080' }}> {/* Dark Royal Blue */}
+      <div className="container-fluid py-4">
+        <div className="d-flex align-items-center justify-content-between">
+
+        {/* Left: Logo */}
+        <div className="d-flex align-items-center">
+            <img 
+              src={logo} 
+              alt="College Logo" 
+              className="logo-custom"
+              style={{ height: '100px', width: 'auto' }}
+            />
+          </div>
+
+          {/* Center: Main Menu */}
+          <div className="text-center flex-grow-1 mx-4">
+            <nav>
+              <ul className="nav justify-content-center mb-1">
+                {['Home', 'About Us', 'Admissions', 'Programs', 'Student Corner'].map((item, index) => (
+                  <li className="nav-item" key={index}>
+                    <a className="nav-link  fw-bold nav-link-custom" href="#">
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <hr className="border-light my-1" style={{ width: '100%' }} />
+            </nav>
+
+            {/* Bottom Menu */}
+            <nav>
+              <ul className="nav justify-content-center mt-1">
+                {['IT @ MIT', 'Placements', 'Faculty', 'Careers', 'Life @ MIT', 'Mumbai', 'News & Events'].map((item, index) => (
+                  <li className="nav-item" key={index}>
+                    <a className="nav-link  fw-bold nav-link-custom" href="#">
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+
+         {/* Right: Apply Now Button */}
+         <div className="d-flex align-items-center">
+            <a href="#" className="btn btn-warning fw-bold px-4 py-2 apply-now-button">
+              Apply Now
+            </a>
+          </div>
+
         </div>
       </div>
-    </nav>
-  </header>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;

947793d6dae56c5f49e275eb53969c5c2cd9186c
jackmitwpu <jay.shah1@mitwpu.edu.in>
2025-04-27 14:02:16 +0530

    welcome message update

--- mit_mumbai_bharat_web/src/pages/Home/Home.jsx
+++ mit_mumbai_bharat_web/src/pages/Home/Home.jsx
@@ -46,13 +46,13 @@ const Home = () => {
             <div className="col-md-6 mb-4 mb-md-0 welcome-text">
               <h2 className="mb-3 text-white ">Welcome to MAEER'S MIT MUMBAI</h2>
               <p className="text-white">
-                With over 40 years of academic excellence and a global alumni network exceeding 100,000, MIT is a premier engineering institute approved by AICTE and affiliated with top Indian universities.
+              With over 40 years of academic excellence and a global alumni network exceeding 100,000, MIT is a premier engineering institute approved by AICTE and affiliated with top Indian universities. Known for its commitment to innovation and quality education, MIT empowers students to meet global challenges through cutting-edge learning experiences.
               </p>
               <p className="text-white">
-                Extending the legacy of MIT Pune, we are proud to launch our new Engineering College in the Mumbai Metropolitan Region from the academic year 2025–2026.
+              Extending the legacy of MIT Pune, we are proud to launch our new Engineering College in the Mumbai Metropolitan Region from the academic year 2025–2026. Set in a nature-inspired campus, the institute offers advanced infrastructure, expert faculty, and a dynamic learning environment that promotes holistic development.
               </p>
               <p className="text-white">
-                At MIT Mumbai, education is more than a degree—it’s a transformative journey toward professional and personal excellence.
+              At MIT Mumbai, education is more than a degree—it's a transformative journey toward professional and personal excellence.
               </p>
             </div>
 

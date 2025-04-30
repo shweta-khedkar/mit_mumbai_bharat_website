46b06bfd39ca5c591939f5db0400dd07efbbcdb6
shweta khedkar <137026859+shweta-khedkar@users.noreply.github.com>
2025-04-30 13:53:46 +0530

    ready electronics page

--- mit_mumbai_bharat_web/src/pages/courses/Courses.jsx
+++ mit_mumbai_bharat_web/src/pages/courses/Courses.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './courses.css';
+import React from "react";
+import "./courses.css";
 import aiml from "../../assets/ai_ml.jpg";
 import it from "../../assets/it.jpg";
 import et from "../../assets/et.jpg";
 import compscience from "../../assets/comp_science.jpg";
+import { Link } from "react-router-dom";
 
 const Courses = () => {
   return (
     <div className="courses-page">
-
       {/* Header */}
       <div id="header-placeholder"></div>
 
@@ -18,13 +18,19 @@ const Courses = () => {
 
         {/* Table Layout */}
         <div className="courses-table">
-
           {/* Row 1 */}
           <div className="courses-row right-image">
             <div className="courses-details">
-              <h5 className="courses-title">Computer Science & Engineering (Artificial Intelligence)</h5>
-              <p className="courses-description">Learn cutting-edge AI, ML, and automation techniques.</p>
-              <a href="#" className="btn btn-explore">Explore More</a>
+              <h5 className="courses-title">
+                Computer Science & Engineering (Artificial Intelligence /
+                Machine Learning)
+              </h5>
+              <p className="courses-description">
+                Learn cutting-edge AI, ML, and automation techniques.
+              </p>
+              <Link to="/programme/aiml" className="btn btn-explore">
+                Explore More
+              </Link>
             </div>
             <div className="courses-img">
               <img src={aiml} alt="CSE AI" />
@@ -34,9 +40,13 @@ const Courses = () => {
           {/* Row 2 */}
           <div className="courses-row left-image">
             <div className="courses-details">
-              <h5 className="courses-title">Computer Science & Engineering (Data Science)</h5>
-              <p className="courses-description">Master big data analytics and predictive modeling.</p>
-              <a href="#" className="btn btn-explore">Explore More</a>
+              <h5 className="courses-title">Computer Science & Engineering</h5>
+              <p className="courses-description">
+                Master big data analytics and predictive modeling.
+              </p>
+              <Link to="/programme/cseds" className="btn btn-explore">
+                Explore More
+              </Link>
             </div>
             <div className="courses-img">
               <img src={compscience} alt="CSE DS" />
@@ -47,8 +57,12 @@ const Courses = () => {
           <div className="courses-row right-image">
             <div className="courses-details">
               <h5 className="courses-title">Electronics Engineering</h5>
-              <p className="courses-description">Design the future with embedded systems and IoT.</p>
-              <a href="#" className="btn btn-explore">Explore More</a>
+              <p className="courses-description">
+                Design the future with embedded systems and IoT.
+              </p>
+              <Link to="/programme/et" className="btn btn-explore">
+                Explore More
+              </Link>
             </div>
             <div className="courses-img">
               <img src={et} alt="Electronics Engineering" />
@@ -59,20 +73,22 @@ const Courses = () => {
           <div className="courses-row left-image">
             <div className="courses-details">
               <h5 className="courses-title">Information Technology</h5>
-              <p className="courses-description">Develop full-stack applications and cloud solutions.</p>
-              <a href="#" className="btn btn-explore">Explore More</a>
+              <p className="courses-description">
+                Develop full-stack applications and cloud solutions.
+              </p>
+              <Link to="/programme/it" className="btn btn-explore">
+                Explore More
+              </Link>
             </div>
             <div className="courses-img">
               <img src={it} alt="Information Technology" />
             </div>
           </div>
-
         </div>
       </div>
 
       {/* Footer */}
       <div id="footer-placeholder"></div>
-
     </div>
   );
 };
